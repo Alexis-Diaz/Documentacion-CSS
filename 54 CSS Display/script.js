dd3e7a76f5b391ea83b7c4b9ab5aa394cafdcfaa
Alexis-Diaz <alex.d4556@gmail.com>
2022-06-22 11:21:11 -0600

    Respaldo inicial del tutorial css

---
+++ 54 CSS Display/script.js
@@ -0,0 +1,16 @@
+function none () {
+    let img = document.getElementById("img1");
+    img.style.display = "none";
+}
+
+function hiddenElement () {
+    let img = document.getElementById("img2");
+    img.style.visibility = "hidden";
+}
+
+function reiniciar () {
+    let img1 = document.getElementById("img1");
+    let img2 = document.getElementById("img2");
+    img1.style.display = "inline";
+    img2.style.visibility = "visible";
+}
